84bc4bb6105683231612002bdc3d2ff2dcafff7d
BaljeetKaur <baljeetkaur7@gmail.com>
2019-03-22 18:01:17 +0530

    These are production files

---
+++ MyProdJavaProject/src/GreetingBaljeet.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class GreetingBaljeet {
+
+	public static void main(String[] args) {
+		Date dateobj = new Date();
+		System.out.println("**** This is production version ***");
+		System.out.println("Baljeet, the date today is " + dateobj);
+	}
+
+}
---
+++ MyProdJavaProject/src/HelloJava.java
@@ -0,0 +1,11 @@
+
+public class HelloJava {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("**** This is production version ***");
+		System.out.println("1. Hello Baljeet");
+		System.out.println("2. Trying to commit this file");
+		System.out.println("3. Testing how staging looks like");
+	}
+}
